654b95c4f50c9396872a1b5414d027006f7a776f
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-12-21 15:35:25 +0200

    Add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+} 
